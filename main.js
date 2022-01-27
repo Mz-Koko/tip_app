fbce0fa3728e2691e1ad9f9af0e917645a51bfe8
Mz-Koko <kiheanetumike@gmail.com>
2022-01-27 18:04:26 +0100

    corrected tip amount calculation

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ btn.addEventListener('click', function(){
     let billAmount = document.getElementById('bill_amount').value;
     let tipPercentage = document.getElementById('tip_percentage').value;
 
-    let tipAmount = document.getElementById('tip_amount').value = billAmount / tipPercentage;
+    let tipAmount = document.getElementById('tip_amount').value = billAmount * tipPercentage / 100;
     document.getElementById('total_bill').value = parseFloat(billAmount) + parseFloat(tipAmount);
 })
